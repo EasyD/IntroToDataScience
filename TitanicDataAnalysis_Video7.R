a01bd03faa6a6e9b0242e8ed5187f4e8f28a3350
Luka Milosevic <lukamilosevic@mail.com>
2017-11-11 05:01:05 +0100

    Update TitanicDataAnalysis_Video7.R

--- TitanicDataAnalysis_Video7.R
+++ TitanicDataAnalysis_Video7.R
@@ -17,9 +17,9 @@
 
 
 #
-# This R source code file corresponds to video 3 of the YouTube series
+# This R source code file corresponds to video 7 of the YouTube series
 # "Introduction to Data Science with R" located at the following URL:
-#     https://youtu.be/aMV_6LmCs4Q
+#     https://youtu.be/fuB7s19g3nQ     
 #
 
 
